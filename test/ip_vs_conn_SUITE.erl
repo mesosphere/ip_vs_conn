eea1a3ea6724e6222c2c3bfd3167c0135bcc3f03
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-10-03 21:51:40 -0700

    gen server

--- test/ip_vs_conn_SUITE.erl
+++ test/ip_vs_conn_SUITE.erl
@@ -6,9 +6,21 @@
 all() -> [test_parse].
 
 test_parse(_Config) ->
-  Root = "../../../../",
+  Proc = ip_vs_conn_config:proc_file(),
   Ret = [{ip_vs_conn,tcp,167792566,47808,167792566,8080,167792566,8081,syn_recv,59,[],[]},
          {ip_vs_conn,tcp,167792566,62061,167792566,8080,167792566,8081,syn_recv,58,[],[]},
          {ip_vs_conn,tcp,167792566,69,167792566,8080,167792566,8081,syn_recv,57,[],[]}],
-  Ret = ip_vs_conn:parse(Root ++ "testdata/proc_ip_vs_conn"),
+  Ret = ip_vs_conn:parse(Proc),
   ok.
+
+init_per_testcase(_, Config) ->
+  Proc = "../../../../testdata/proc_ip_vs_conn",
+  application:set_env(ip_vs_conn, interval_seconds, 1),
+  application:set_env(ip_vs_conn, proc_file, Proc),
+  {ok, _} = application:ensure_all_started(ip_vs_conn),
+  Config.
+
+end_per_testcase(_, _Config) ->
+  ok = application:stop(ip_vs_conn).
+
+
